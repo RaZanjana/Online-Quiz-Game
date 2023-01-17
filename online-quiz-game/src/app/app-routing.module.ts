f46a51c81aa8247eb0eeb2453ce9480bd9304d95
23707 <23707@srilankan.com>
2023-01-17 15:47:17 +0530

    login screen

--- online-quiz-game/src/app/app-routing.module.ts
+++ online-quiz-game/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/quiz-game/sign-in',
+    pathMatch:'full'
+  },
+  {
+    path:'quiz-game',
+    loadChildren:()=>import('./user/user.module').then(mod=>mod.UserModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
